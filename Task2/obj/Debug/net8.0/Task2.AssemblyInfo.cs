0a55e2dee95863a232e1b82eaefd9523a142339b
Veronika <gavdur.veronika@gmail.ru>
2024-03-13 14:38:45 +0300

    add quotient2 lesson2

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c133ce1cd0d3ca05c334f2a81c4d0e7ef7ff75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2207b8a53fc4b97b05743dbae45a3bc190f27d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

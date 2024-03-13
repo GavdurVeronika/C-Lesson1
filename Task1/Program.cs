fd0d13ebc14f32c3304423c0bdaa776702bbf82f
Veronika <gavdur.veronika@gmail.ru>
2024-03-13 13:38:09 +0300

    add under push1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,7 +7,9 @@ Console.WriteLine(value);
 double d = 5.55;
 char c = '!';
 bool b = true;
+bool e = true;
 
 Console.WriteLine(d);
 Console.WriteLine(c);
 Console.WriteLine(b);
+Console.WriteLine(e);
